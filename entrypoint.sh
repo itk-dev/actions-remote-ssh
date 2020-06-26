4425881972295f93de680086b1dbfba80b9e44d4
Jesper Kristensen <cableman@linuxdev.dk>
2020-06-26 14:43:40 +0200

    SERV-336: Fixed public key

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,10 @@ SSHPATH="$HOME/.ssh"
 mkdir -p "$SSHPATH"
 touch "$SSHPATH/known_hosts"
 
-echo "$INPUT_KEY" > "$SSHPATH/deploy_key"
+echo "$INPUT_KEY" > "$SSHPATH/id_rsa-cert.pub"
 chmod 700 "$SSHPATH"
 chmod 600 "$SSHPATH/known_hosts"
-chmod 600 "$SSHPATH/id_rsa"
+chmod 600 "$SSHPATH/id_rsa-cert.pub"
 
 echo "$INPUT_COMMAND exit" > /sshcmd.sh
 cat sshcmd.sh
@@ -18,4 +18,4 @@ cat sshcmd.sh
 echo Start Run Command
 
 
-sh -c "ssh $INPUT_ARGS -o StrictHostKeyChecking=no -p $INPUT_PORT -l ${INPUT_USER} ${INPUT_HOST} < sshcmd.sh"
+sh -c "ssh $INPUT_ARGS -i $SSHPATH/id_rsa-cert.pub -o StrictHostKeyChecking=no -p $INPUT_PORT -l ${INPUT_USER} ${INPUT_HOST} < sshcmd.sh"
