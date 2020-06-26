881f27aa59f69ee7e13b8f55718f886a0dccc654
Jesper Kristensen <cableman@linuxdev.dk>
2020-06-26 15:16:46 +0200

    SERV-336: Added ssh agent

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,24 @@
 set -eu
 
 SSHPATH="$HOME/.ssh"
-
 mkdir -p "$SSHPATH"
-touch "$SSHPATH/known_hosts"
 
+# Setup ssh keys.
 echo "$INPUT_KEY" > "$SSHPATH/id_rsa"
 echo "$INPUT_CERT" > "$SSHPATH/id_rsa-cert.pub"
 chmod 700 "$SSHPATH"
-chmod 600 "$SSHPATH/known_hosts"
 chmod 600 "$SSHPATH/id_rsa"
 chmod 600 "$SSHPATH/id_rsa-cert.pub"
 
+# Create known hosts.
+touch "$SSHPATH/known_hosts"
+chmod 600 "$SSHPATH/known_hosts"
+
+# Start agent and add cert
+ssh-agent -s
+ssh-add
+
+# Build remote command.
 echo "$INPUT_COMMAND exit" > /sshcmd.sh
 cat /sshcmd.sh
 
